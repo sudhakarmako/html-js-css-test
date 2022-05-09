4cb7eb9a327eaf02d3c3dfd36e62e86c37db65fc
sampath1911 <104055029+sampath1911@users.noreply.github.com>
2022-05-09 17:57:49 +0530

    xtra mark

---
+++ xtra mark/app.js
@@ -0,0 +1,26 @@
+function leastCommonMultiple(min, max) {
+    function range(min, max) {
+        var arr = [];
+        for (var i = min; i <= max; i++) {
+            arr.push(i);
+        }
+        return arr;
+    }
+
+    function gcd(a, b) {
+        return !b ? a : gcd(b, a % b);
+    }
+
+    function lcm(a, b) {
+        return (a * b) / gcd(a, b);   
+    }
+
+    var multiple = min;
+    range(min, max).forEach(function(n) {
+        multiple = lcm(multiple, n);
+    });
+
+    document.getElementById('result').innerHTML=multiple;
+}
+
+leastCommonMultiple(1, 13); // => 360360
