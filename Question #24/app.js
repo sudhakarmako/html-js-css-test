86ccd543d8d4ac33a369d7ec189f2d9935a3b802
sudhakarmako <sudhakar@makoitlab.com>
2022-05-11 12:22:33 +0530

    manjari

--- Question #24/app.js
+++ Question #24/app.js
@@ -1,23 +1,11 @@
-// var firstWord = "Mary";
-// var secondWord = "Army";
+var firstword="mary";
+var secondword="army";
+console.log(isAnagram(firstword,secondword));
 
-// // call a function to check if given two words are Anagrams
-// isAnagram(firstWord, secondWord); // Output is: true
-
-// function isAnagram(first, second) {
-//   // For case insensitivity, change both words to lowercase.
-//   var a = first.toLowerCase();
-//   var b = second.toLowerCase();
-
-//   console.log(a.split("")) // returns array
-//   console.log(a.split("").sort()) // sort an array
-//   console.log(a.split("").sort().join("")) // joins back & returns string
-  
-//   // Sort the strings, and join the resulting array to a string. Compare the results
-//   a = a.split("").sort().join("");
-//   // apply same to second word
-//   b = b.split("").sort().join("");
-
-//   // compare and return result
-//   return a === b;
-// }
+function isAnagram(first,second){
+  var a= first.toLowerCase();
+  var b= second.toLowerCase();
+  a=a.split("").sort().join("");
+  b=b.split("").sort().join("");
+   return a===b;
+}
--- xtra mark/app.js
+++ xtra mark/app.js
@@ -1,17 +1,13 @@
-// program to find the LCM of two integers
 
-// take input
-const num1 = prompt('Enter a first positive integer: ');
-const num2 = prompt('Enter a second positive integer: ');
+let hcf;
+const number1 = prompt('Enter a first positive integer: ');
+const number2 = prompt('Enter a second positive integer: ');
 
-// higher number among number1 and number2 is stored in min
-let min = (num1 > num2) ? num1 : num2;
+for (let i = 1; i <= number1 && i <= number2; i++) {
 
-// while loop
-while (true) {
-    if (min % num1 == 0 && min % num2 == 0) {
-        console.log(`The LCM of ${num1} and ${num2} is ${min}`);
-        break;
+    if( number1 % i == 0 && number2 % i == 0) {
+        hcf = i;
     }
-    min++;
 }
+let lcm = (number1 * number2) / hcf;
+console.log(`HCF of ${number1} and ${number2} is ${lcm}.`);
